44df4ac4e9fc8ab77147f4d642ef60318aa5c664
Pedro Luiz <pedroluizlsg@gmail.com>
2022-08-05 00:19:12 -0300

    Requisito 5

--- src/App.js
+++ src/App.js
@@ -15,47 +15,66 @@ class App extends Component {
     this.state = {
       isSaveButtonDisabled: true,
       inputValue: '',
+      isSearchButtonDisabled: true,
     };
   }
 
-handleChange = ({ target: { name, value } }) => {
-  this.setState(() => ({ [name]: value }), this.handleButtonDisable);
-};
+  handleChange = ({ target: { name, value } }) => {
+    console.log(name, value);
+    this.setState(() => ({ [name]: value }), this.handleButtonDisable);
+  };
 
-handleButtonDisable = () => {
-  const { inputValue } = this.state;
-  const minLength = 3;
-  this.setState({ isSaveButtonDisabled: (inputValue.length < minLength) });
-}
+  handleButtonDisable = () => {
+    const { inputValue } = this.state;
+    const minLength = 3;
+    const mLengthSearch = 2;
+    this.setState({
+      isSaveButtonDisabled: (inputValue.length < minLength),
+      isSearchButtonDisabled: (inputValue.length < mLengthSearch),
+    });
+  }
 
-render() {
-  const { isSaveButtonDisabled, inputValue } = this.state;
-  return (
-    <BrowserRouter>
-      <p className="title">TrybeTunes</p>
-      <Switch>
-        <Route
-          exact
-          path="/"
-          render={ (props) => (
-            <Login
-              { ...props }
-              handleChange={ this.handleChange }
-              isSaveButtonDisabled={ isSaveButtonDisabled }
-              inputValue={ inputValue }
-            />
-          ) }
-        />
-        <Route exact path="/search" component={ Search } />
-        <Route exact path="/album/:id" component={ Album } />
-        <Route exact path="/favorites" component={ Favorites } />
-        <Route exact path="/profile" component={ Profile } />
-        <Route exact path="/profile/edit" component={ ProfileEdit } />
-        <Route path="" component={ NotFound } />
-      </Switch>
-    </BrowserRouter>
-  );
-}
+  render() {
+    const { isSaveButtonDisabled, inputValue, isSearchButtonDisabled } = this.state;
+    return (
+      <BrowserRouter>
+        <p className="title">TrybeTunes</p>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={ (props) => (
+              <Login
+                { ...props }
+                handleChange={ this.handleChange }
+                isSaveButtonDisabled={ isSaveButtonDisabled }
+                inputValue={ inputValue }
+              />
+            ) }
+          />
+
+          <Route
+            exact
+            path="/search"
+            render={ (props) => (
+              <Search
+                { ...props }
+                handleChange={ this.handleChange }
+                inputValue={ inputValue }
+                isSearchButtonDisabled={ isSearchButtonDisabled }
+              />
+            ) }
+          />
+
+          <Route exact path="/album/:id" component={ Album } />
+          <Route exact path="/favorites" component={ Favorites } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="" component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,15 +1,45 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Search extends Component {
   render() {
+    const {
+      handleChange,
+      inputValue,
+      isSearchButtonDisabled,
+    } = this.props;
     return (
-      <div data-testid="page-search">
-        <Header />
-        Página Search
-      </div>
+      <section>
+        <div data-testid="page-search">
+          <Header />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="inputValue"
+            onChange={ handleChange }
+            value={ inputValue }
+            data-testid="search-artist-input"
+          />
+
+          <button
+            type="button"
+            disabled={ isSearchButtonDisabled }
+            data-testid="search-artist-button"
+          >
+            Pesquisar
+          </button>
+        </div>
+      </section>
     );
   }
 }
+
+Search.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  inputValue: PropTypes.string.isRequired,
+  isSearchButtonDisabled: PropTypes.bool.isRequired,
+};
 
 export default Search;
